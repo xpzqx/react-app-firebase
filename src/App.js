92f0a6afe72510cfedafbdbd4d45e45721928608
yemiwebby <yemiwebby@gmail.com>
2022-07-12 22:06:47 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    const getData = async() => {
+      const demoData = await axios.get( "https://jsonplaceholder.typicode.com/posts");
+      setPosts(demoData.data);
+    }
+
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+            <div className="row">
+        <div className="col-md-12 text-center" style={{ margin: "50px 0" }}>
+          <h1> Fake Posts from JSON Placeholder</h1>
+        </div>
+      </div>
+
+      <div className="row">
+        {posts &&
+          posts.map((post) => (
+            <div className="card col-md-4" key={post.id}>
+              <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+                <p className="card-text">{post.body}</p>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
